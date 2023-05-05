7d8abf056546726dce3bc3b2d6e8527989c74f93
didzc11 <didacdomenechgarcia@gmail.com>
2023-05-05 11:33:35 +0200

    new breaking changes adding new features

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,30 @@
-local colorscheme = "catppuccin"
+local M = {
+  "catppuccin/nvim",
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+}
 
-local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
-if not status_ok then
-  return
+M.name = "catppuccin"
+function M.config()
+
+  -- # day/night theme checker #
+  local current_hour = tonumber(os.date("%H"))
+
+  if current_hour >= 6 and current_hour < 18 then
+    FLAVOUR = "latte"
+  else
+    FLAVOUR = "mocha"
+  end
+
+  require("catppuccin").setup({
+    flavour = FLAVOUR
+  })
+
+  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+  if not status_ok then
+    return
+  end
+
 end
+
+return M
